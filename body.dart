9adb48c741c956f71365bbfbe96048061adc5e61
Jahin Tasnia <jahintasnia15@gmail.com>
2025-05-19 15:41:41 +0600

    Create body.dart

---
+++ body.dart
@@ -0,0 +1,76 @@
+// Body
+
+      /*
+      body: Text("Hi Girls!"),
+      */
+
+
+      /*
+      body: Center(
+        child: Text("Hi Girls!"),
+      ),
+      */
+
+
+      /*
+      body: Center(
+        child: Image.network("https://media.licdn.com/dms/image/v2/D5603AQHiHspJ_hZEOw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1730772826375?e=2147483647&v=beta&t=pmotjUZF0jvBwV6Je_3hYt2BGR9N6ob-1wx86813OJ4"),
+      ),
+       */
+
+
+      /*
+      body: Container(
+        height: 250,
+        width: 250,
+        alignment: Alignment.topCenter,
+        margin: EdgeInsets.fromLTRB(40, 90, 10, 40),
+        padding: EdgeInsets.fromLTRB(60, 50, 20, 10),
+        //child: Text("Hello Girls!"),
+        decoration: BoxDecoration(
+          color: Colors.green,
+          border: Border.all(
+            color: Colors.white38,
+            width: 6,
+          ),
+        ),
+        child: Image.network("https://media.licdn.com/dms/image/v2/D5603AQHiHspJ_hZEOw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1730772826375?e=2147483647&v=beta&t=pmotjUZF0jvBwV6Je_3hYt2BGR9N6ob-1wx86813OJ4"),
+        
+      ),
+      
+       */
+
+
+      /*
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(height: 100,width: 100,child: Image.network("https://media.licdn.com/dms/image/v2/D5603AQHiHspJ_hZEOw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1730772826375?e=2147483647&v=beta&t=pmotjUZF0jvBwV6Je_3hYt2BGR9N6ob-1wx86813OJ4"),),
+          Container(height: 100,width: 100,child: Image.network("https://media.licdn.com/dms/image/v2/D5603AQHiHspJ_hZEOw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1730772826375?e=2147483647&v=beta&t=pmotjUZF0jvBwV6Je_3hYt2BGR9N6ob-1wx86813OJ4"),),
+          Container(height: 100,width: 100,child: Image.network("https://media.licdn.com/dms/image/v2/D5603AQHiHspJ_hZEOw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1730772826375?e=2147483647&v=beta&t=pmotjUZF0jvBwV6Je_3hYt2BGR9N6ob-1wx86813OJ4"),),
+        ],
+      ),
+      
+       */
+      
+      
+      /*
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          TextButton(onPressed: (){MySnackbar("I'm Text button", context);}, child: Text("Submit")),
+          ElevatedButton(onPressed: (){MySnackbar("I'm Elevated button", context);}, child: Text("Submit"),style: buttonStyle,),
+          OutlinedButton(onPressed: (){MySnackbar("I'm Outline button", context);}, child: Text("Button")),
+        ],
+      ),
+      
+       */
+
+
+     /*
+      body: Center(
+        child: ElevatedButton(onPressed: (){MyAlertDialog(context);}, child: Text("Click me"),style: ButtonStyle(),),
+      ),
+
+      */
+      
